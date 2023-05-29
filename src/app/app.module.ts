9d85a0cba8d0f9b3f072f4ef33b446dbeeb8c72d
EzraG101 <ezga923@gmail.com>
2023-05-29 16:52:45 -0400

    router stuff, home page, and not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
-  declarations: [AppComponent, PageComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    PageComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    HomeComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent],
