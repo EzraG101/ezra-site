0874337b69f71ca03bf6bf132022242cc4b0ee5e
EzraG101 <ezga923@gmail.com>
2023-05-29 14:18:20 -0400

    add routing stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +9,7 @@ import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
+const routes: Routes = [];
 @NgModule({
   declarations: [AppComponent, PageComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
