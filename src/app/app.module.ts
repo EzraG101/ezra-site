e2ea7ff8eb09c7b99c7327c7c1b899af0720dbc6
EzraG101 <ezga923@gmail.com>
2023-05-30 11:18:45 -0400

    icons and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -19,7 +21,13 @@ import { HomeComponent } from './home/home.component';
     NotFoundComponent,
     HomeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
