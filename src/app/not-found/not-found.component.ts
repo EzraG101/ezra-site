8d72a5db90e33ca48d8da10355bc5c63dafcbea9
EzraG101 <ezga923@gmail.com>
2023-06-10 18:25:36 -0400

    oops, hack ruined 404, fixed!

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
 })
 export class NotFoundComponent {
-
+  ngOnInit() {
+    document.querySelector('.hide')?.setAttribute('hidden', 'true');
+  }
 }
