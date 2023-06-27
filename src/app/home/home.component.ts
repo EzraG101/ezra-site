128f26532c5b432eeb8c36bf3becb1d68f7413a4
EzraG101 <ezga923@gmail.com>
2023-06-27 18:21:30 -0400

    fixed home lets go

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,20 +12,17 @@ export class HomeComponent {
   // }
   ngOnInit() {
     // document.querySelector('.hide')?.setAttribute('hidden', 'false');
-    const firstTime = localStorage.getItem('key');
-    if (!firstTime) {
-      localStorage.setItem('key', 'loaded');
-      location.reload();
-    } else {
-      localStorage.removeItem('key');
-    }
-
-    let card = document.querySelector('.card');
-    this.height = card?.clientHeight;
-
-    console.log(card);
-    console.log(this.height);
-
-    document.querySelector('.hide')?.setAttribute('hidden', 'true');
+    // const firstTime = localStorage.getItem('key');
+    // if (!firstTime) {
+    //   localStorage.setItem('key', 'loaded');
+    //   location.reload();
+    // } else {
+    //   localStorage.removeItem('key');
+    // }
+    // let card = document.querySelector('.card');
+    // this.height = card?.clientHeight;
+    // console.log(card);
+    // console.log(this.height);
+    // document.querySelector('.hide')?.setAttribute('hidden', 'true');
   }
 }
