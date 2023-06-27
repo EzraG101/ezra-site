afe8bc630383bf39d38ee1dfec320276c177322f
EzraG101 <ezga923@gmail.com>
2023-06-27 13:55:32 -0400

    /folletos routs properly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
+import { FolletosComponent } from './folletos/folletos.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: '/folletos', component: FolletosComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
