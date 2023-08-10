261f918202de67e047c172c04598fd23f886cb4a
EzraG101 <ezga923@gmail.com>
2023-08-09 22:10:49 -0400

    test de muchos posts

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-// import { MathjaxComponent } from '../mathjax/mathjax.component';
+import { MathjaxComponent } from '../mathjax/mathjax.component';
 
 @Component({
   selector: 'app-blog',
@@ -7,8 +7,18 @@ import { Component, ViewChild } from '@angular/core';
   styleUrls: ['./blog.component.css'],
 })
 export class BlogComponent {
-  //   @ViewChild(MathjaxComponent) childView: MathjaxComponent;
-  //   name = 'Mathjax ';
+  @ViewChild(MathjaxComponent) childView: MathjaxComponent;
+  name = 'Mathjax ';
+  blogs = [
+    {
+      content: `Test 1: $\\frac{1}{2}+\\frac{1}{3}=\\frac{5}{6}$.`,
+      date: '09/08/2023',
+    },
+    {
+      content: `Test 2: $1+1=2$.`,
+      date: '09/08/2023',
+    },
+  ];
   //   mathContent = `When $ a \\ne 0 $, there are two solutions to \\(ax^2 + bx + c = 0 \\) and they are
   // $$ x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}$$`;
 }
